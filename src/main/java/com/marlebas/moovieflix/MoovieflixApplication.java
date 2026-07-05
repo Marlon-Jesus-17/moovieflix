43cf5d97b444ee2dfa5fd9630e0efd47210cac08
Marlon <maarlonsantos17@gmail.com>
2026-07-05 16:56:37 -0300

    primeiro commit

---
+++ src/main/java/com/marlebas/moovieflix/MoovieflixApplication.java
@@ -0,0 +1,13 @@
+package com.marlebas.moovieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoovieflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoovieflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlebas/moovieflix/MoovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlebas.moovieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
